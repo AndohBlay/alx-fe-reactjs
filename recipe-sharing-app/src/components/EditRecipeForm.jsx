620b185989b380343c845abb658d8ba3d95a6504
Blay <godandblay@gmail.com>
2025-09-19 19:36:16 +0200

    Added a new file

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-import { useRecipeStore } from '../components/recipeStore';
+import { useRecipeStore } from '../stores/recipeStore';
 
 const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore((state) => state.updateRecipe);
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
     updateRecipe({ ...recipe, title, description });
   };
 
